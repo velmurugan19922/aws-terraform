6c689cc231bba55844347c375394b29b24fddafd
velu <veluonhere@gmail.com>
2023-06-07 09:28:52 +0530

    create private subnet A

--- vpc.tf
+++ vpc.tf
@@ -29,3 +29,14 @@ resource "aws_subnet" "aws-big-pubsubnet-B" {
     Name = "aws-big-pubsubnet-B"
   }
 }
+# create subnet private-A
+resource "aws_subnet" "aws-big-pvtsubnet-A" {
+  vpc_id     = aws_vpc.aws-big-vpc.id
+  availability_zone = "eu-north-1a"
+  map_public_ip_on_launch = "true"
+  cidr_block = "10.0.0.0/20"
+
+  tags = {
+    Name = "aws-big-pvtsubnet-A"
+  }
+}
