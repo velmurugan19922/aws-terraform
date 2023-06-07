b52473f8329187dd87c3a0a10cb665617b795ed4
velu <veluonhere@gmail.com>
2023-06-07 09:20:29 +0530

    change name

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "aws-big-vpc" {
     Name = "aws-big-vpc"
   }
 }
+# create subnet public-A
+resource "aws_subnet" "aws-big-pubsubnet-A" {
+  vpc_id     = aws_vpc.aws-big-vpc.id
+  availability_zone = "eu-north-1a"
+  map_public_ip_on_launch = "true"
+  cidr_block = "10.0.0.0/20"
+
+  tags = {
+    Name = "aws-big-pubsubnet-A"
+  }
+}
