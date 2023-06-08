c26e78a6df29cc71070361ea10544241e8f180ee
velu <veluonhere@gmail.com>
2023-06-08 22:46:29 +0530

    change

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "small-ec2" {
   ami           = "ami-0430580de6244e02e"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.aws-big-pubsubnet-A
+  subnet_id = aws_subnet.aws-big-pubsubnet-A.id
   vpc_security_group_ids = [aws_security_group.big-sg.id]
   key_name = "aws-newkey"
   user_data = file("webapp.sh")
