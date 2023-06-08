4fca979c9b53903ab8f8a745b7eabdeb2050011d
velu <veluonhere@gmail.com>
2023-06-08 22:45:14 +0530

    create ec2

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+# Setup EC2 Instance
+resource "aws_instance" "small-ec2" {
+  ami           = "ami-0430580de6244e02e"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.aws-big-pubsubnet-A
+  vpc_security_group_ids = [aws_security_group.big-sg.id]
+  key_name = "aws-newkey"
+  user_data = file("webapp.sh")
+  tags = {
+    Name = "small-ec2"
+  }
+}
