4211bd52bc66ee0c1afd8970c65f3400139e57a0
velu <veluonhere@gmail.com>
2023-06-08 23:05:20 +0530

    change

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # Setup EC2 Instance
 resource "aws_instance" "small-ec2" {
-  ami           = "ami-0430580de6244e02e"
-  instance_type = "t2.micro"
+  ami           = "ami-0989fb15ce71ba39e"
+  instance_type = "t3.micro"
   subnet_id = aws_subnet.aws-big-pubsubnet-A.id
   vpc_security_group_ids = [aws_security_group.big-sg.id]
   key_name = "aws-newkey"
