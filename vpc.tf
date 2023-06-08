9fb0f7ec92f855ef6c0c013e2a4599da13a1f371
velu <veluonhere@gmail.com>
2023-06-08 22:10:39 +0530

    create subet ass

--- vpc.tf
+++ vpc.tf
@@ -84,3 +84,59 @@ resource "aws_route_table" "big-pvtrt" {
     Name = "big-pvtrt"
   }
 }
+#public subnet association-a
+resource "aws_route_table_association" "pub-sub-ass-a" {
+  subnet_id      = aws_subnet.aws-big-pubsubnet-A.id
+  route_table_id = aws_route_table.big-pubrt.id
+}
+#public subnet association-b
+resource "aws_route_table_association" "pub-sub-ass-b" {
+  subnet_id      = aws_subnet.aws-big-pubsubnet-B.id
+  route_table_id = aws_route_table.big-pubrt.id
+}
+#private subnet association-a
+resource "aws_route_table_association" "pvt-sub-ass-a" {
+  subnet_id      = aws_subnet.aws-big-pvtsubnet-A.id
+  route_table_id = aws_route_table.big-pvtrt.id
+}
+#private subnet association-b
+resource "aws_route_table_association" "pvt-sub-ass-b" {
+  subnet_id      = aws_subnet.aws-big-pvtsubnet-B.id
+  route_table_id = aws_route_table.big-pvtrt.id
+}
+# Security Group - SSH & HTTP
+resource "aws_security_group" "big-sg" {
+  name        = "allow_web"
+  description = "Allow SSH & HTTP inbound traffic"
+  vpc_id      = aws_vpc.aws-big-vpc.id
+
+  ingress {
+    description      = "SSH from www"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+   ingress {
+    description      = "HTTP from www"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "big-sg"
+  }
+}
